243b5a1da4afc4837d68691603110aa57c6fe648
Kraynov108 <Registrattionsmail@gmail.com>
2024-12-17 12:39:26 +0300

    Add files via upload
    Домашняя работа по уроку "Стиль кода часть II. Цикл While"

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+variable = 0
+while variable < len(my_list):
+    if my_list[variable] < 0:
+        break
+    if my_list[variable] > 0:
+        print(my_list[variable])
+    variable += 1
